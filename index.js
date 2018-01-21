b035276fd102c9d2e003b45824cf94103e46851b
johnny_shaman <johnny_shaman@live.jp>
2018-01-21 04:05:11 +0000

    new file:   index.js

---
+++ index.js
@@ -0,0 +1,37 @@
+(() =>{
+    "use strict";
+
+    const is = Object.assign((t) => {
+        try {
+            return t.constructor;
+        } catch (e) {
+            return t;
+        }
+    }, {
+        "array": (t) => is(t) === Array,
+        "blank": (t) => t === "",
+        "boolean": (t) => is(t) === Boolean,
+        "defined": (t) => is(t) !== undefined,
+        "function": (t) => is(t) === Function,
+        "generator": (t) => is(t) === is(function* (){}),
+        "iterable": (t) => is(t) === is(function* (){}()),
+        "held": (t) => (p) => t instanceof p,
+        "nan": (t) => is.number(t) && isNaN(t),
+        "null": (t) => is(t) === null,
+        "number": (t) => is(t) === Number,
+        "object": (t) => is(t) === Object,
+        "pure": (t) => is.object(t) || is.array(t),
+        "self": (t) => is.valid(t) && is(t).prototype === t,
+        "string": (t) => is(t) === String,
+        "symbol": (t) => is(t) === Symbol,
+        "there": (t) => t.length !== 0,
+        "valid": (t) => !is.blank(t) && !is.nan(t) && is(t) !== t,
+        "client": this.constructor.name === "Window",
+        "server": this.constructor.name !== "Window",
+        "wroker": Object.getPrototypeOf(this.constructor.prototype).constructor.name === "WorkerGlobalScope"
+    });
+
+    is.worker && (self.is = is);
+    is.server && (module.exports = is);
+    is.client && (this.is = is);
+})();
