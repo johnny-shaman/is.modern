6b32ef71acc63ca0663ff38c49c9d99adf398c0e
johnny_shaman <johnny_shaman@live.jp>
2018-01-23 01:07:52 +0000

    new file:   is.modern.js 	modified:   package.json 	deleted:    index.js 	.npmignore

---
+++ is.modern.js
@@ -0,0 +1,34 @@
+(() => {
+    "use strict";
+    const is = Object.assign((t) => {
+        try {
+            return t.constructor;
+        } catch (e) {
+            return t;
+        }
+    }, {
+        "array": (t) => is(t) === Array,
+        "blank": (t) => t === "",
+        "boolean": (t) => is(t) === Boolean,
+        "defined": (t) => is(t) !== undefined,
+        "function": (t) => is(t) === Function,
+        "generator": (t) => is(t) === is(function* (){}),
+        "iterable": (t) => is(t) === is(function* (){}()),
+        "held": (t) => (p) => t instanceof p,
+        "nan": (t) => is.number(t) && isNaN(t),
+        "null": (t) => is(t) === null,
+        "number": (t) => is(t) === Number,
+        "object": (t) => is(t) === Object,
+        "pure": (t) => is.object(t) || is.array(t),
+        "self": (t) => is.valid(t) && is(t).prototype === t,
+        "string": (t) => is(t) === String,
+        "symbol": (t) => is(t) === Symbol,
+        "there": (t) => Object.keys(t).length !== 0,
+        "valid": (t) => !is.nan(t) && !is.blank(t) && is(t) !== t,
+        "client": this.constructor.name === "Window",
+        "server": this.constructor.name !== "Window",
+    });
+
+    is.server && (module.exports = is);
+    is.client && (window.is = is);
+})();
